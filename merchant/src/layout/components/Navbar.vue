e4396283afdac2d7bfc194cdf5f6bf862da86cc8
雷雪松 <leixs@idss-cn.com>
2020-07-20 17:42:24 +0800

    增加修改密码

--- merchant/src/layout/components/Navbar.vue
+++ merchant/src/layout/components/Navbar.vue
@@ -12,7 +12,7 @@
         </div>
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
           <router-link to="/">
-            <el-dropdown-item>
+            <el-dropdown-item @click.native="infoDialogVisible = true">
               修改密码
             </el-dropdown-item>
           </router-link>
@@ -22,6 +22,30 @@
         </el-dropdown-menu>
       </el-dropdown>
     </div>
+    <!--修改密码-->
+    <el-dialog
+      append-to-body
+      title="修改密码"
+      :close-on-click-modal="false"
+      :close-on-press-escape="false"
+      :before-close="handleClose"
+      :visible.sync="infoDialogVisible">
+      <el-form ref="pwdForm" :rules="rules" :model="pwdForm"  label-width="130px" >
+        <el-form-item label="原密码" prop="oldPassword" >
+          <el-input clearable v-model="pwdForm.oldPassword" show-password/>
+        </el-form-item>
+        <el-form-item label="新密码" prop="newPassword">
+          <el-input clearable v-model="pwdForm.newPassword" show-password/>
+        </el-form-item>
+        <el-form-item label="确认新密码" prop="confirmUserPwd">
+          <el-input clearable v-model="pwdForm.confirmUserPwd" show-password />
+        </el-form-item>
+        <el-form-item>
+          <el-button @click="handleClose">取消</el-button>
+          <el-button type="primary" @click="submitForm">保存</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
@@ -41,6 +65,21 @@ export default {
       'user'
     ])
   },
+  data(){
+    return {
+      infoDialogVisible: false,
+      pwdForm: {
+        oldPassword: '',
+        newPassword: '',
+        confirmUserPwd: ''
+      },
+      rules: {
+        oldPassword: [{ required: true, message: '请输入原密码', trigger: 'blur' }],
+        newPassword: [{ required: true, message: '请输入新密码', trigger: 'blur' }],
+        confirmUserPwd: [{ required: true, message: '请再次输入新密码', trigger: 'blur' }]
+      }
+    }
+  },
   methods: {
     toggleSideBar() {
       this.$store.dispatch('app/toggleSideBar')
@@ -48,6 +87,39 @@ export default {
     async logout() {
       await this.$store.dispatch('user/logout')
       this.$router.push(`/login?redirect=${this.$route.fullPath}`)
+    },
+    handleClose (done) {
+      this.$refs.pwdForm.resetFields()
+      this.infoDialogVisible = false
+      typeof done === 'function' && done()
+    },
+    async submitForm () {
+      this.$refs.pwdForm.validate(async (valid) => {
+        if (valid) {
+          let result = await this.$request({
+            url: `/user/modify`,
+            method: 'post',
+            tag: 'list',
+            data: this.pwdForm
+          })
+          if (result) {
+            this.$message({
+              showClose: true,
+              message: '密码修改成功！',
+              type: 'success'
+            })
+            this.handleClose()
+          } else {
+            this.$message({
+              showClose: true,
+              message: result.message,
+              type: 'error'
+            })
+          }
+        } else {
+          return false
+        }
+      })
     }
   }
 }
